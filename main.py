bb7cb214657edd2308eaa352ee28eacb64065659
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-11-25 10:36:45 +0530

    removed .pyc

--- main.py
+++ main.py
@@ -95,16 +95,12 @@ def algo_over_all_dataset(alpha=0.85, n_iter=1000):
     datasets = ["Reddit-2", "IMDB", "Wikipedia", "IISc"]
     files = ["./dataset/Reddit-graph-int-2.graphml", "./dataset/IMDB-graph-int-2.graphml", "./dataset/Wikipedia-graph-int-2.graphml", "./dataset/iisc-graph-int-2.graphml"]
 
-    # Initialize data storage
     all_runtimes = []
     for name, filepath in zip(datasets[:], files[:]):
         G = nx.read_graphml(filepath)
         G_sp, int2node_sp, W = Graph.make_graph(G)
-
-        # Initialize variables for storing run times
         runtimes = []
 
-        # Timing `Standard_PageRank`
         start = time.time()
         std_rank, std_l2 = Standard_PageRank(G_sp, int2node_sp, alpha, n_iter).run()
         end = time.time()
@@ -112,7 +108,6 @@ def algo_over_all_dataset(alpha=0.85, n_iter=1000):
         runtimes.append(paper_time)
         print(f"Standard : Done (Iteration {len(std_l2)})")
 
-        # Timing `Weighted_PageRank`
         start = time.time()
         w_rank, w_l2 = Weighted_PageRank(G_sp, W, alpha=alpha, int2node=int2node_sp).run()
         end = time.time()
@@ -120,7 +115,6 @@ def algo_over_all_dataset(alpha=0.85, n_iter=1000):
         runtimes.append(weighted_time)
         print(f"Weighted : Done (Iteration {len(w_l2)})")
 
-        # Timing `Simplified_PageRank`
         start = time.time()
         sim_rank, sim_l2 = Simplified_PageRank(G_sp, iteration=n_iter).run()
         end = time.time()
@@ -128,7 +122,6 @@ def algo_over_all_dataset(alpha=0.85, n_iter=1000):
         runtimes.append(simplified_time)
         print(f"Simplified : Done (Iteration {len(sim_l2)})")
 
-        # Timing `networkx Simple` PageRank
         start = time.time()
         nx_pagerank_sim = nx.pagerank(G_sp, alpha=1, max_iter=n_iter)
         end = time.time()
@@ -136,7 +129,6 @@ def algo_over_all_dataset(alpha=0.85, n_iter=1000):
         runtimes.append(nx_time)
         print("Networkx Simple : Done")
 
-        # Timing `networkx Standard` PageRank
         start = time.time()
         nx_pagerank_std = nx.pagerank(G_sp, alpha=alpha, max_iter=n_iter)
         end = time.time()
@@ -144,7 +136,6 @@ def algo_over_all_dataset(alpha=0.85, n_iter=1000):
         runtimes.append(nx_time)
         print("Network Standard : Done")
 
-        # Timing `networkx Weighted` PageRank
         start = time.time()
         nx_pagerank_wtd = nx.pagerank(nx.from_numpy_array(W, create_using=nx.DiGraph), alpha=alpha, max_iter=n_iter, weight='weight')
         end = time.time()
@@ -154,10 +145,6 @@ def algo_over_all_dataset(alpha=0.85, n_iter=1000):
 
         # Store runtimes for the current dataset
         all_runtimes.append(runtimes)
-
-        # nx_pagerank_sim = nx.pagerank(G_sp, alpha=1, max_iter=n_iter)
-        # nx_pagerank_std = nx.pagerank(G_sp, alpha=alpha, max_iter=n_iter)
-
         top_k = 10
         # Convert networkx pagerank result to format compatible with int2node_sp
         nx_rank_sim = [(node, score) for node, score in nx_pagerank_sim.items()]
@@ -165,28 +152,28 @@ def algo_over_all_dataset(alpha=0.85, n_iter=1000):
         nx_rank_wtd = [(node, score) for node, score in nx_pagerank_wtd.items()]
 
 
-        top_5_nx_sim_rank = sorted(nx_rank_sim, key=lambda x: x[1], reverse=True)[:top_k]
-        top_5_nx_std_rank = sorted(nx_rank_std, key=lambda x: x[1], reverse=True)[:top_k]
-        top_5_nx_wtd_rank = sorted(nx_rank_wtd, key=lambda x: x[1], reverse=True)[:top_k]
+        top_k_nx_sim_rank = sorted(nx_rank_sim, key=lambda x: x[1], reverse=True)[:top_k]
+        top_k_nx_std_rank = sorted(nx_rank_std, key=lambda x: x[1], reverse=True)[:top_k]
+        top_k_nx_wtd_rank = sorted(nx_rank_wtd, key=lambda x: x[1], reverse=True)[:top_k]
 
-        top_5_standard = get_top_n_ranks(std_rank, int2node_sp, top_k)
-        top_5_weighted = get_top_n_ranks(w_rank, int2node_sp, top_k)
-        top_5_simplified = get_top_n_ranks(sim_rank, int2node_sp, top_k)
+        top_k_standard = get_top_n_ranks(std_rank, int2node_sp, top_k)
+        top_k_weighted = get_top_n_ranks(w_rank, int2node_sp, top_k)
+        top_k_simplified = get_top_n_ranks(sim_rank, int2node_sp, top_k)
 
         # Header for the table
         print(f"Dataset - Top {top_k} Ranks Comparison")
-        print(f"{'Rank':<5} {'Standard':<25} {'Weighted':<25} {'Simplified':<25} {'NetworkX (Simple)':<25} {'NetworkX (Standard)':<25} {'NetworkX (Weighted)':<25}")
+        print(f"{'Rank':<5} {'Simplified':<25}  {'NetworkX (Simple)':<25} {'Standard':<25} {'NetworkX (Standard)':<25} {'Weighted':<25} {'NetworkX (Weighted)':<25}")
         print('-' * 135)
 
         # Display top 5 for each algorithm
         for i in range(top_k):
-            standard = f"{top_5_standard[i][0]}: {top_5_standard[i][1]:.8f}"
-            weighted = f"{top_5_weighted[i][0]}: {top_5_weighted[i][1]:.8f}"
-            simplified = f"{top_5_simplified[i][0]}: {top_5_simplified[i][1]:.8f}"
-            nx_sim = f"{top_5_nx_sim_rank[i][0]}: {top_5_nx_sim_rank[i][1]:.8f}"
-            nx_std = f"{top_5_nx_std_rank[i][0]}: {top_5_nx_std_rank[i][1]:.8f}"
-            nx_wtd = f"{top_5_nx_wtd_rank[i][0]}: {top_5_nx_wtd_rank[i][1]:.8f}"
-            print(f"{i + 1:<5} {standard:<25} {weighted:<25} {simplified:<25} {nx_sim:<25} {nx_std:<25} {nx_wtd:<25}")
+            standard = f"{top_k_standard[i][0]}: {top_k_standard[i][1]:.8f}"
+            weighted = f"{top_k_weighted[i][0]}: {top_k_weighted[i][1]:.8f}"
+            simplified = f"{top_k_simplified[i][0]}: {top_k_simplified[i][1]:.8f}"
+            nx_sim = f"{top_k_nx_sim_rank[i][0]}: {top_k_nx_sim_rank[i][1]:.8f}"
+            nx_std = f"{top_k_nx_std_rank[i][0]}: {top_k_nx_std_rank[i][1]:.8f}"
+            nx_wtd = f"{top_k_nx_wtd_rank[i][0]}: {top_k_nx_wtd_rank[i][1]:.8f}"
+            print(f"{i + 1:<5} {simplified:<25} {nx_sim:<25} {standard:<25} {nx_std:<25} {weighted:<25} {nx_wtd:<25}")
         print('-' * 135)
         
     # Plotting the grouped bar chart
